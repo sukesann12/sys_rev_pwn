79295acedd1f1b32588516a7a845cf751a361cb2
sukesann12 <82489343+sukesann12@users.noreply.github.com>
2021-06-21 00:51:54 +0900

    Add

---
+++ ls.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <err.h>
+
+int main(int argc, char **argv){
+	char *target_path;
+
+	if (argc > 1)
+		target_path = argv[1];
+	else
+		target_path = ".";
+
+	DIR *target = opendir(target_path);
+	if (target == NULL)
+		err(1, "opendir");
+
+	struct dirent *d;
+
+	while ((d = readdir(target)) != NULL){
+		printf("%s\n", d->d_name);
+	}
+}
